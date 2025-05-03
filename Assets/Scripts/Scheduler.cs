e1e9d25d9444e521ad42351a2b56db63d4d46552
Melkor <melkor5456@gmail.com>
2025-05-03 14:07:39 -0400

    push

--- Assets/Scripts/Scheduler.cs
+++ Assets/Scripts/Scheduler.cs
@@ -6,7 +6,8 @@ using System.Linq;
 using SectionID = System.Tuple<int, int>; // (courseID, sectionID)
 using BlockID = System.Tuple<int, int>;
 using System.Security.Cryptography;
-using JetBrains.Annotations; // (day, blockID)
+using JetBrains.Annotations;
+using System; // (day, blockID)
 class PartialSchedule {
     public List<SectionID> sections = new List<SectionID>(); // list of sections in the schedule
     public SectionID[,] timeBlocks;
@@ -25,6 +26,7 @@ class PartialSchedule {
         // check if there are any overlapping time blocks
         foreach (BlockID block in classInfo.timeblock) {
             if (timeBlocks[(int)block.Item1, (int)block.Item2].Item1 != -1) {
+                Debug.Log("Overlapping time block found: " + block.Item1 + " " + block.Item2);
                 return false; // overlapping time block found
             }
         }
@@ -50,14 +52,15 @@ public class Scheduler : MonoBehaviour
     private List<PartialSchedule> schedules = new List<PartialSchedule>(); // list of all schedules
     private void Awake()
     {
-        scheduleData = GameManager.instance.scheduleData;
-        scheduleData.CreateTimeBlocks();
-        // Make sure this object is not destroyed when loading a new scene
-        DontDestroyOnLoad(gameObject);
+        
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        scheduleData = GameManager.instance.scheduleData;
+        scheduleData.CreateTimeBlocks();
+        // Make sure this object is not destroyed when loading a new scene
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
@@ -65,16 +68,34 @@ public class Scheduler : MonoBehaviour
     {
         
     }
+
+    private void printSchedule(PartialSchedule schedule) {
+        String repr = "Schedule: \n";
+        for (int i = 0; i < schedule.timeBlocks.GetLength(0); i++) {
+            for (int j = 0; j < schedule.timeBlocks.GetLength(1); j++) {
+                if (schedule.timeBlocks[i, j].Item1 != -1) {
+                    String str = schedule.timeBlocks[i, j].Item1.ToString() + " " + schedule.timeBlocks[i, j].Item2.ToString();
+                    //write to console without new line
+                    repr += str.PadLeft(5, '0') + " ";
+                } else {
+                    repr += ("xxxxx ");
+                }
+            }
+            repr += "\n";
+        }
+        Debug.Log(repr);
+    }
+                    
     public void generateSchedules() {
         List<ClassInfo> stack = new List<ClassInfo>();
         List<int> classIDs = new List<int>();
         stack.Add(scheduleData.courses[0].classes[0]); // add the first class to the stack
         classIDs.Add(0); // add the first class ID to the list
         PartialSchedule schedule = new PartialSchedule(scheduleData.numBlocks);
-        schedule.AddSection(stack[0], 0, 0); // add the first class to the schedule
         while (stack.Count < scheduleData.courses.Count) {
                 int courseID = stack.Count -  1; // get the course ID of the course we are currently adding
                 if (schedule.isValid(stack.Last())) {
+                    Debug.Log("Added class " + scheduleData.courses[stack.Count - 1].name + " " + stack.Last().name + " to schedule"); // print the name of the class that was  added
                     // if the schedule is valid, add the next class to the stack
                     schedule.AddSection(stack.Last(), courseID, classIDs.Last()); // add the first class to the schedule
                     if (courseID + 1 >= scheduleData.courses.Count) {
@@ -82,15 +103,18 @@ public class Scheduler : MonoBehaviour
                         Debug.Log("Found a valid schedule!");
                         schedules.Add(schedule); // add the schedule to the list of schedules
                         //TODO: backtrack to find all schedules
-                        break;
+                        return;
                     }
                     classIDs.Add(0); // add the first class ID to the list
                     stack.Add(scheduleData.courses[courseID+1].classes[0]); // add the first class to the stack
+                    printSchedule(schedule); // print the schedule to the console
                 } else {
+                    Debug.Log("Failed to add class " + scheduleData.courses[stack.Count - 1].name + " " + stack.Last().name + " to schedule"); // print the name of the class that failed to be added
                     while (classIDs.Last() + 1 >= scheduleData.courses[classIDs.Count - 1].classes.Count) {
                         // if the class ID is out of range, remove the last class from the stack and try the next class
                         stack.RemoveAt(stack.Count - 1); // remove the last class from the stack
                         classIDs.RemoveAt(classIDs.Count - 1); // remove the last class ID from the list
+                        schedule.RemoveLastSection(stack.Last()); // remove the last class from the schedule
                         if (stack.Count == 0) {
                             // if there are no more classes in the stack, we have found all schedules
                             Debug.Log("Found all schedules!");
@@ -101,6 +125,11 @@ public class Scheduler : MonoBehaviour
                     classIDs[classIDs.Count - 1]++; // increment the class ID
                     stack[stack.Count - 1] = scheduleData.courses[classIDs.Count - 1].classes[classIDs.Last()]; // add the next class to the stack
                 }
+        }
+        if (schedules.Count == 0) {
+            Debug.Log("No valid schedules found!");
+        } else {
+            Debug.Log("Found " + schedules.Count + " valid schedules!");
         }
     }
 }
