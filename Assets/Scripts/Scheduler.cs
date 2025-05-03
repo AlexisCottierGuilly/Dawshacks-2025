ac88824b36842873904413240e791ae3f281c44d
Melkor <melkor5456@gmail.com>
2025-05-03 13:05:48 -0400

    add basic functionality of schedule generation

--- Assets/Scripts/Scheduler.cs
+++ Assets/Scripts/Scheduler.cs
@@ -1,16 +1,106 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+
+using SectionID = System.Tuple<int, int>; // (courseID, sectionID)
+using BlockID = System.Tuple<int, int>;
+using System.Security.Cryptography;
+using JetBrains.Annotations; // (day, blockID)
+class PartialSchedule {
+    public List<SectionID> sections = new List<SectionID>(); // list of sections in the schedule
+    public SectionID[,] timeBlocks;
+
+    public PartialSchedule(int numBlocks) {
+        timeBlocks = new SectionID[5, numBlocks]; // 5 days in a week
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < numBlocks; j++) {
+                timeBlocks[i, j] = new SectionID(-1, -1); // initialize all time blocks to null
+            }
+        }
+    }
+
+    public bool isValid(ClassInfo classInfo) {
+        // check if the schedule is valid
+        // check if there are any overlapping time blocks
+        foreach (BlockID block in classInfo.timeblock) {
+            if (timeBlocks[(int)block.Item1, (int)block.Item2].Item1 != -1) {
+                return false; // overlapping time block found
+            }
+        }
+        return true; // no overlapping time blocks found
+    }
+    public void AddSection(ClassInfo classInfo, int courseID, int sectionID) {
+        sections.Add(new SectionID(courseID, sectionID));
+        foreach (BlockID block in classInfo.timeblock) {
+            timeBlocks[(int)block.Item1, (int)block.Item2] = new SectionID(courseID, sectionID);
+        }
+    }
+    public void RemoveLastSection(ClassInfo classInfo) {
+        foreach (BlockID block in classInfo.timeblock) {
+            timeBlocks[(int)block.Item1, (int)block.Item2] = new SectionID(-1, -1);
+        }
+        sections.RemoveAt(sections.Count - 1); // remove the last section from the schedule
+    }
+}
 
 public class Scheduler : MonoBehaviour
 {
+    private ScheduleData scheduleData;
+    private List<PartialSchedule> schedules = new List<PartialSchedule>(); // list of all schedules
+    private void Awake()
+    {
+        scheduleData = GameManager.instance.scheduleData;
+        scheduleData.CreateTimeBlocks();
+        // Make sure this object is not destroyed when loading a new scene
+        DontDestroyOnLoad(gameObject);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void generateSchedules() {
+        List<ClassInfo> stack = new List<ClassInfo>();
+        List<int> classIDs = new List<int>();
+        stack.Add(scheduleData.courses[0].classes[0]); // add the first class to the stack
+        classIDs.Add(0); // add the first class ID to the list
+        PartialSchedule schedule = new PartialSchedule(scheduleData.numBlocks);
+        schedule.AddSection(stack[0], 0, 0); // add the first class to the schedule
+        while (stack.Count < scheduleData.courses.Count) {
+                int courseID = stack.Count -  1; // get the course ID of the course we are currently adding
+                if (schedule.isValid(stack.Last())) {
+                    // if the schedule is valid, add the next class to the stack
+                    schedule.AddSection(stack.Last(), courseID, classIDs.Last()); // add the first class to the schedule
+                    if (courseID + 1 >= scheduleData.courses.Count) {
+                        // if we have added all the classes, we have found a valid schedule
+                        Debug.Log("Found a valid schedule!");
+                        schedules.Add(schedule); // add the schedule to the list of schedules
+                        //TODO: backtrack to find all schedules
+                        break;
+                    }
+                    classIDs.Add(0); // add the first class ID to the list
+                    stack.Add(scheduleData.courses[courseID+1].classes[0]); // add the first class to the stack
+                } else {
+                    while (classIDs.Last() + 1 >= scheduleData.courses[classIDs.Count - 1].classes.Count) {
+                        // if the class ID is out of range, remove the last class from the stack and try the next class
+                        stack.RemoveAt(stack.Count - 1); // remove the last class from the stack
+                        classIDs.RemoveAt(classIDs.Count - 1); // remove the last class ID from the list
+                        if (stack.Count == 0) {
+                            // if there are no more classes in the stack, we have found all schedules
+                            Debug.Log("Found all schedules!");
+                            return;
+                        }
+                    }
+                    // if the class ID is in range, add the next class to the stack
+                    classIDs[classIDs.Count - 1]++; // increment the class ID
+                    stack[stack.Count - 1] = scheduleData.courses[classIDs.Count - 1].classes[classIDs.Last()]; // add the next class to the stack
+                }
+        }
     }
 }
--- Assets/Scripts/Scriptable/ScheduleData.cs
+++ Assets/Scripts/Scriptable/ScheduleData.cs
@@ -1,10 +1,55 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+
+using BlockID = System.Tuple<int, int>; // (day, blockID)
 
 [CreateAssetMenu(fileName = "ScheduleData", menuName = "Data/ScheduleData")]
 public class ScheduleData : ScriptableObject
 {
-    public List<CourseInfo> classes = new List<CourseInfo>();
+    public List<CourseInfo> courses = new();
+    private int earliestStartTime = 24 * 60; // 24 hours in minutes
+    private int latestEndTime = 0; // 0 minutes
+    public int numBlocks = 0; // number of 30 minute blocks in a day
+    int HourminToMinutes(Vector2Int time) {
+        return time.x * 60 + time.y;
+    }
+    int MinToIdx(int minutes) {
+        return (minutes - earliestStartTime) / 30;
+    }
+    public void CreateTimeBlocks() {
+        //first find earliest start time and latest end time
+        foreach (CourseInfo course in courses) {
+            foreach (ClassInfo classInfo in course.classes) {
+                foreach (TimeSlotInfo timeSlot in classInfo.timeSlots) {
+                    int startTime = HourminToMinutes(timeSlot.startTime.time);
+                    int endTime = HourminToMinutes(timeSlot.endTime.time);
+                    if (startTime < earliestStartTime) {
+                        earliestStartTime = startTime;
+                    }
+                    if (endTime > latestEndTime) {
+                        latestEndTime = endTime;
+                    }
+                }
+            }
+        }
+        numBlocks = (latestEndTime - earliestStartTime) / 30;
+        //convert each time slot to a timeblock
+        foreach (CourseInfo course in courses) {
+            foreach (ClassInfo classInfo in course.classes) {
+                foreach (TimeSlotInfo timeSlot in classInfo.timeSlots) {
+                    //create a timeblock for each time slot
+                    int startIdx = MinToIdx(HourminToMinutes(timeSlot.startTime.time));
+                    int endIdx = MinToIdx(HourminToMinutes(timeSlot.endTime.time));
+                    for (int i = startIdx; i < endIdx; i++) {
+                        classInfo.timeblock.Add(new BlockID((int)timeSlot.day, i));
+                    }
+                }
+            }
+        }   
+    }
+    
+
 }
 
 
@@ -12,7 +57,7 @@ public class ScheduleData : ScriptableObject
 public class CourseInfo
 {
     public string name;  // Bio
-    public List<ClassInfo> classes = new List<ClassInfo>();
+    public List<ClassInfo> classes = new();
 }
 
 [System.Serializable]
@@ -21,8 +66,8 @@ public class ClassInfo
     public string name;  // Bio-134
     public string teacher;  // Dr. Smith
     public string location;
-
-    public List<TimeSlotInfo> timeSlots = new List<TimeSlotInfo>();
+    public List<Tuple<int, int>> timeblock = new();
+    public List<TimeSlotInfo> timeSlots = new();
 
 }
 
